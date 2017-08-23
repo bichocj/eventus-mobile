673e825ea2d285077d43d7444f4e59f3c1ddf37d
bicho <bichocj@gmail.com>
2017-08-23 11:14:36 -0500

    fix sync with server

--- app/src/main/java/Objects/NumberResponse.java
+++ app/src/main/java/Objects/NumberResponse.java
@@ -1,8 +1,11 @@
 package Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by Administrador on 23/04/2017.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NumberResponse {
     ActivityProximate activities;
     RegisterProximate registers;
